745080c01687796cd32d92455a50dd8982b81bed
prudhvi <prudhvi.d123@gmail.com>
2013-06-15 11:45:13 -0700

    Using latest version of mongo and bson_ext

--- cab_app.rb
+++ cab_app.rb
@@ -11,7 +11,7 @@ configure do
   #set :mongo_db, conn.db('cabs')
   mongo_uri = ENV['MONGOLAB_URI']
   db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
-  client = Mongo::Connection.from_uri(mongo_uri)
+  client = MongoClient.from_uri(mongo_uri)
   set :mongo_db, client.db(db_name)
   settings.mongo_db['cabs'].ensure_index({location: "2dsphere"})
 end
