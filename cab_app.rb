4306ff5557e3df9972714434f50d834b395b8eb5
prudhvi <prudhvi.d123@gmail.com>
2013-06-15 11:19:12 -0700

    Mongo support for heroku

--- cab_app.rb
+++ cab_app.rb
@@ -7,8 +7,12 @@ require './cab'
 include Mongo
 
 configure do
-  conn = MongoClient.new("localhost", 27017)
-  set :mongo_db, conn.db('cabs')
+  #conn = MongoClient.new("localhost", 27017)
+  #set :mongo_db, conn.db('cabs')
+  mongo_uri = ENV['MONGOLAB_URI']
+  db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+  client = MongoClient.from_uri(mongo_uri)
+  set :mongo_db, client.db(db_name)
   settings.mongo_db['cabs'].ensure_index({location: "2dsphere"})
 end
 
